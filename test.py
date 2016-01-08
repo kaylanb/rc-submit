3775e6a7b98646f235a9a7d582ea0b6e0d1a71b7
Kaylan Burleigh <kaylanb@berkeley.edu>
2016-01-08 16:18:25 -0500

    MVP for presentation of 1 node job submission script to group

--- test.py
+++ test.py
@@ -3,20 +3,18 @@ import rcsubmit
 import time
 
 sc = rcsubmit.StarCluster("smallcluster")
-
 # Simple ssh command example
-sc.ssh('echo "hello world" > test')
+#sc.ssh('echo "hello world" > test')
 
 # Copying files and running remote script
 sc.push_files(['./test.sh'])
-sc.ssh('cd {} && sh test.sh > hello_world.out'.format(sc.remote_dir))
-sc.pull_files(['hello_world.out'])
+#sc.ssh('cd {} && sh test.sh > hello_world.out'.format(sc.remote_dir))
+#sc.pull_files(['hello_world.out'])
 
 # Using scheduler
 sc.ssh('cd {} && qsub -V -cwd test.sh'.format(sc.remote_dir))
-time.sleep(10)
+sleep 5
 sc.pull_files(['hello_world.out'])
-
 
 #submission = rcsubmit.Submission(sc, 'qsub', 'job.pbs', 'test.sh')
 #submission.run()
